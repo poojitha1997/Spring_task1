2efe1a1185ab56ea9898b1df3fab185a03f778a9
Sai Poojitha <pujita.pabbathi@gmail.com>
2019-07-12 09:39:00 +0530

    Fourth task

--- src/main/java/com/stackroute/Application.java
+++ src/main/java/com/stackroute/Application.java
@@ -1,7 +1,10 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -9,12 +12,9 @@ public class Application
 {
     public static void main( String[] args )
     {
-        //using ApplicationContext to create the bean
-        ApplicationContext context = new ClassPathXmlApplicationContext("main/resources/beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie = context.getBean("movieid",Movie.class);
         movie.display();
-        Movie movie2 = context.getBean("movieid2",Movie.class);
-        movie2.display();
 
 
 
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,10 +1,19 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 import java.util.List;
 
-public class Movie
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware
 {
     //list of objects of type Actor
+    private ApplicationContext applicationContext = null;
+
 
     public Movie()
     {
@@ -30,4 +39,23 @@ public class Movie
         System.out.println(actor.toString());
     }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        System.out.println(applicationContext);
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("bean is"+s);
+
+    }
 }
